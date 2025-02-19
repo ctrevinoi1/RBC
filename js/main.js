b100350200b7ce084903890652f10962d1184f7d
Ciaran Trevino <ciarantrevino@mac.lan>
2025-02-19 14:00:02 -0800

    Initial website for RBC Cleaning LLC

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// js/main.js
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("RBC Cleaning LLC website loaded.");
+    // You can add interactive JavaScript here if needed.
+  });
+  
